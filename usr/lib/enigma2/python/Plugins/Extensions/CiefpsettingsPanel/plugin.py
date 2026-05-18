f996ec87d7d6cc881153e90d47dbe3b59dd383e9
ciefp <35612046+ciefp@users.noreply.github.com>
2026-05-18 20:56:08 +0200

    Update 6.4

--- usr/lib/enigma2/python/Plugins/Extensions/CiefpsettingsPanel/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/CiefpsettingsPanel/plugin.py
@@ -24,7 +24,7 @@ import tarfile
 logging.basicConfig(filename="/tmp/ciefp_install.log", level=logging.DEBUG, format="%(asctime)s - %(message)s")
 
 # Verzija plugina
-PLUGIN_VERSION = "6.3"
+PLUGIN_VERSION = "6.4"
 
 # URL za proveru verzije
 VERSION_URL = "https://raw.githubusercontent.com/ciefp/CiefpsettingsPanel/refs/heads/main/version.txt"
@@ -83,6 +83,7 @@ PLUGINS = {
     "CiefpEasySetup": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/CiefpEasySetup/main/installer.sh -O - | /bin/sh",
     "CiefpPicturePlayer": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/CiefpPicturePlayer/main/installer.sh -O - | /bin/sh",
     "CiefpVideoPlayer": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/CiefpVideoPlayer/main/installer.sh -O - | /bin/sh",
+    "CiefpYouTube": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/CiefpYouTube/main/installer.sh -O - | /bin/sh",
     "TitloviBrowser": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/TitloviBrowser/main/installer.sh -O - | /bin/sh",
     "WebCamE2PrenjSF": "wget -q --no-check-certificate https://raw.githubusercontent.com/ciefp/WebCamE2PrenjSF/main/installer.sh -O - | /bin/sh",
     "############ ( Panels ) ############": "", 
